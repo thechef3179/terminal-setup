1e2b6d43964d5c5e192b0bc5dc781d14e32c0670
The Chef <thechef3179@gmail.com>
2025-04-27 19:22:38 -0400

    Update keybinds.lua

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -39,3 +39,5 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
+-- keybind for neotree
+vim.keymap.set('n', '<leader>t', '<Cmd>Neotree toggle<CR>')
