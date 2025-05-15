f91c1b4bfe9b359d2c14a95a0d32764a49f91b5d
The Chef <thechef3179@gmail.com>
2025-05-14 20:27:13 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,18 +1,19 @@
 #! /bin/zsh
 
 # update zshrc
-cp ~/Documents/Codes/terminal-setup/ohmyzsh/zshrc ~/.zshrc
+cp ./ohmyzsh/zshrc ~/.zshrc
 
 # update neovim
 mkdir -p ~/.config/nvim/{after,lua}
 mkdir -p ~/.config/nvim/after/ftplugin
 mkdir -p ~/.config/nvim/lua/{plugins,config}
-cp ~/Documents/Codes/terminal-setup/nvim/init.lua ~/.config/nvim/init.lua
-cp ~/Documents/Codes/terminal-setup/nvim/after/ftplugin/* ~/.config/nvim/after/plugin/*
-cp ~/Documents/Codes/terminal-setup/nvim/lua/keybinds.lua ~/.config/nvim/lua/keybinds.lua
-cp ~/Documents/Codes/terminal-setup/nvim/lua/lsp.lua ~/.config/nvim/lua/lsp.lua
-cp ~/Documents/Codes/terminal-setup/nvim/lua/config/* ~/.config/nvim/lua/config/
-cp ~/Documents/Codes/terminal-setup/nvim/lua/plugins/* ~/.config/nvim/lua/plugins/
+cp ./nvim/init.lua ~/.config/nvim/init.lua
+cp ./nvim/after/ftplugin/* ~/.config/nvim/after/ftplugin/*
+cp ./nvim/lua/keybinds.lua ~/.config/nvim/lua/keybinds.lua
+cp ./nvim/lua/lsp.lua ~/.config/nvim/lua/lsp.lua
+cp ./nvim/lua/config/* ~/.config/nvim/lua/config/
+cp ./nvim/lua/plugins/* ~/.config/nvim/lua/plugins/
 
 # update tmux
-cp ~/Documents/Codes/terminal-setup/tmux/tmux.conf ~/.tmux.conf
+cp ./tmux/tmux.conf ~/.tmux.conf
+
