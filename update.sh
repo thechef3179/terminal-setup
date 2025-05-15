3500d89d4b9e00770917974042fda99c133cc76f
Neelabh <n0kshe01@gmail.com>
2025-05-15 10:38:38 -0400

    made mkdir more efficient in update.sh

--- update.sh
+++ update.sh
@@ -4,9 +4,8 @@
 cp ./ohmyzsh/zshrc ~/.zshrc
 
 # update neovim
-mkdir -p ~/.config/nvim/{after,lua}
-mkdir -p ~/.config/nvim/after/ftplugin
 mkdir -p ~/.config/nvim/lua/{plugins,config}
+mkdir -p ~/.config/nvim/after/ftplugin
 cp ./nvim/init.lua ~/.config/nvim/init.lua
 cp ./nvim/after/ftplugin/* ~/.config/nvim/after/ftplugin/
 cp ./nvim/lua/*.lua ~/.config/nvim/lua/
