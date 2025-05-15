d3763bf09659c76107dfdcc1669596613c5c5a00
The Chef <thechef3179@gmail.com>
2025-05-14 20:34:41 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -9,8 +9,7 @@ mkdir -p ~/.config/nvim/after/ftplugin
 mkdir -p ~/.config/nvim/lua/{plugins,config}
 cp ./nvim/init.lua ~/.config/nvim/init.lua
 cp ./nvim/after/ftplugin/* ~/.config/nvim/after/ftplugin/
-cp ./nvim/lua/keybinds.lua ~/.config/nvim/lua/keybinds.lua
-cp ./nvim/lua/lsp.lua ~/.config/nvim/lua/lsp.lua
+cp ./nvim/lua/*.lua ~/.config/nvim/lua/
 cp ./nvim/lua/config/* ~/.config/nvim/lua/config/
 cp ./nvim/lua/plugins/* ~/.config/nvim/lua/plugins/
 
