a1cc67fa23f35756dca7a9edaa883470297c9c85
The Chef <thechef3179@gmail.com>
2025-05-19 12:00:35 -0400

    some change to nvim config to allow JK to navigate when tex or markdown

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,7 @@
+-- create remap for JK for moving entire unbroken line
+vim.keymap.set('n', 'J', 'j', { noremap = true, silent = true })
+vim.keymap.set('n', 'K', 'k', { noremap = true, silent = true })
+
 vim.keymap.set('n', 'j', 'gj', { noremap = true, silent = true })
 vim.keymap.set('n', 'k', 'gk', { noremap = true, silent = true })
 
