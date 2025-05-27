35ec7199b5b4ba2fc78987e0baed163cd74a1771
The Chef <thechef3179@gmail.com>
2025-05-26 23:38:27 -0400

    Updated zshrc with some useful plugins

--- update.sh
+++ update.sh
@@ -3,6 +3,12 @@
 # update zshrc
 cp ./ohmyzsh/zshrc ~/.zshrc
 
+# install plugins for .zsh-autosuggestions and .zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
+
+
 # update neovim
 mkdir -p ~/.config/nvim/lua/{plugins,config}
 mkdir -p ~/.config/nvim/after/ftplugin
