707fc21e1e4fc7686dbc11c7653700dae57077a6
Neelabh <n0kshe01@gmail.com>
2025-05-28 04:04:57 -0400

    Update on tmux for ressurect sessions

--- update.sh
+++ update.sh
@@ -3,6 +3,9 @@
 # update zshrc
 cp ./ohmyzsh/zshrc ~/.zshrc
 
+# install tpm
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 # update neovim
 mkdir -p ~/.config/nvim/lua/{plugins,config}
 mkdir -p ~/.config/nvim/after/ftplugin
