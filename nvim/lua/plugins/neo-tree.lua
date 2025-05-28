b9106d3179d0e91ca70a6a448fc9a5fc567d2617
The Chef <thechef3179@gmail.com>
2025-05-28 05:12:39 -0400

    update neotree to show hidden files by default

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -15,5 +15,12 @@ return {
     window = {
       width = 25,
     },
+    filesystem = {
+        filtered_items = {
+                visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+                hide_dotfiles = true,
+                hide_gitignored = true,
+        },
+    },
   },
 }
