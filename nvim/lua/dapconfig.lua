ff4f7a06fdf84a1e906e10d242b7d5946da93266
The Chef <thechef3179@gmail.com>
2025-06-01 08:20:30 -0400

    autocomplete needs to be invoked, fixed tabbing inconsistency for neovim

--- nvim/lua/dapconfig.lua
+++ nvim/lua/dapconfig.lua
@@ -9,10 +9,10 @@ dappy.setup()
 
 -- configure for dapui event listeners
 dap.listeners.before.attach.dapui_config = function()
- dapui.open()
+    dapui.open()
 end
 dap.listeners.before.launch.dapui_config = function()
- dapui.open()
+    dapui.open()
 end
 
 vim.keymap.set('n', '<Leader>r', function() require('dap').continue() end)
@@ -23,46 +23,49 @@ vim.keymap.set('n', '<Leader>w', function() dapui.toggle() end)
 
 -- setup for c
 dap.adapters.lldb = {
-  type = 'executable',
-  command = '/home/thechef/.local/share/nvim/mason/bin/codelldb',
-  name = 'lldb'
+    type = 'executable',
+    command = '/home/thechef/.local/share/nvim/mason/bin/codelldb',
+    name = 'lldb'
 }
 dap.configurations.c = {
-  {
-    name = 'Launch',
-    type = 'lldb',
-    request = 'launch',
-    program = function()
-      local source_file = vim.fn.input('Name of source code: ', 'main.c')
-      local bin_file = string.sub(source_file, 0, -3)
-      local cwd_ = vim.fn.getcwd()
-      local compile_command = string.format("g++ %s/src/%s -o %s/target/%s", cwd_, source_file, cwd_, bin_file)
-      os.execute(compile_command)
-      return string.format("%s/target/%s", cwd_, bin_file)
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-    args = {},
-  },
+    {
+        name = 'Launch',
+        type = 'lldb',
+        request = 'launch',
+        program = function()
+            local source_file = vim.fn.input('Name of source code: ', 'main.c')
+            local bin_file = string.sub(source_file, 0, -3)
+            local cwd_ = vim.fn.getcwd()
+            local compile_command = string.format("g++ %s/src/%s -o %s/target/%s", cwd_, source_file, cwd_, bin_file)
+            os.execute(compile_command)
+            return string.format("%s/target/%s", cwd_, bin_file)
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = {},
+    },
 }
 dap.configurations.cpp = {
-  {
-    name = 'Launch',
-    type = 'lldb',
-    request = 'launch',
-    program = function()
-      local source_file = vim.fn.input('Name of source code: ', 'main.cpp')
-      local bin_file = string.sub(source_file, 0, -3)
-      local cwd_ = vim.fn.getcwd()
-      local compile_command = string.format("g++ %s/src/%s -o %s/target/%s", cwd_, source_file, cwd_, bin_file)
-      os.execute(compile_command)
-      return string.format("%s/target/%s", cwd_, bin_file)
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-    args = {},
-  },
+    {
+        name = 'Launch',
+        type = 'lldb',
+        request = 'launch',
+        program = function()
+            local source_file = vim.fn.input('Name of source code: ', 'main.cpp')
+            local bin_file = string.sub(source_file, 0, -3)
+            local cwd_ = vim.fn.getcwd()
+            local compile_command = string.format("g++ %s/src/%s -o %s/target/%s", cwd_, source_file, cwd_, bin_file)
+            os.execute(compile_command)
+            return string.format("%s/target/%s", cwd_, bin_file)
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = {},
+    },
 }
 dap.configurations.py = {
     dappy.setup(".venv*/bin/python")
 }
+
+
+
--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,26 +1,29 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  lazy = false, -- neo-tree will lazily load itself
-  ---@module "neo-tree"
-  ---@type neotree.Config?
-  opts = {
-    -- fill any relevant options here
-    window = {
-      width = 25,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
     },
-    filesystem = {
-        filtered_items = {
+    lazy = false, -- neo-tree will lazily load itself
+    ---@module "neo-tree"
+    ---@type neotree.Config?
+    opts = {
+        -- fill any relevant options here
+        window = {
+            width = 25,
+        },
+        filesystem = {
+            filtered_items = {
                 visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
                 hide_dotfiles = true,
                 hide_gitignored = true,
+            },
         },
     },
-  },
 }
+
+
+
