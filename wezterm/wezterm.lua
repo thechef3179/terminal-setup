4a4799e598cc2b6188c3da33c586fd88cbcb8c72
The Chef <thechef3179@gmail.com>
2025-06-11 19:53:23 -0400

    updated the wezterm config for style

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,16 +4,30 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- add your font here
+-- setting window padding
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+
+-- setting color scheme for wezterm
+config.color_scheme = "ayu"
+-- config.color_scheme = "GruvboxDarkHard"
+
+config.font = wezterm.font 'Monofur Nerd Font'
+config.font_size = 24
 config.font = wezterm.font 'OpenDyslexicM Nerd Font Mono'
-config.font_size = 20
+config.font_size = 18
+-- config.font = wezterm.font 'SauceCodePro Nerd Font Mono'
 
 config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.6
-config.window_background_blur = 10
+-- config.window_background_opacity = 0.6
+-- config.macos_window_background_blur = 10
 
 -- and finally, return the configuration to wezterm
 return config
