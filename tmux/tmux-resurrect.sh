4dea495591516dd19c660422bbb4ebfca7da7ebf
The Chef <thechef3179@gmail.com>
2025-06-12 00:45:25 -0400

    more fix on tmux-resurrect.sh for new sessions

--- tmux/tmux-resurrect.sh
+++ tmux/tmux-resurrect.sh
@@ -5,23 +5,28 @@ IFS="#"
 
 current_session=""
 for new_pane in $all_panes; do
-    # echo $new_pane
     IFS=";" read -ra pane_info<<<"$new_pane"
     session=${pane_info[0]}
     window=${pane_info[1]}
     directory=${pane_info[2]}
     if [ ! "$current_session" = "$session" ]; then
-        # echo $session
         current_session="$session"
+        echo "session name: $current_session"
         window_count=0
-        echo "creating new session $session"
-        # tmux new-session -d -s $session
-        tmux new-window -t $session:$(( window_count+1 )) -n $window -c $directory
-        window_count=$(( window_count+1 ))
+        # check if session exists
+        tmux has-session -t $session 2>/dev/null
+        if [ $? != 0 ]; then
+            echo "creating new session $session"
+            tmux new-session -d -s $session
+            tmux new-window -t $session:$(( window_count+2 )) -n $window -c ${directory//\"/}
+            tmux kill-window -t $session:1
+            window_count=$(( window_count+1 ))
+        else
+            tmux new-window -t $session:$(( window_count+1 )) -n $window -c ${directory//\"/}
+            window_count=$(( window_count+1 ))
+        fi
     else 
         tmux new-window -t $session:$(( window_count+1 )) -n $window -c ${directory//\"/}
         window_count=$(( window_count+1 ))
-        # tmux new-window -t $session: -n $window -c $directory
     fi
-    # echo "creating new window $window in $directory"
 done
