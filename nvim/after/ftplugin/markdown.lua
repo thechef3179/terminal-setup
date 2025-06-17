43174a9e8e53006810f3f8cbd6b35ac76537aa47
The Chef <thechef3179@gmail.com>
2025-06-17 06:40:57 -0400

    added a text width for latex and markdown to trigger new line instead of using gj gk remap

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,8 +1,10 @@
--- create remap for JK for moving entire unbroken line
-vim.keymap.set('n', 'J', 'j', { noremap = true, silent = true })
-vim.keymap.set('n', 'K', 'k', { noremap = true, silent = true })
-
-vim.keymap.set('n', 'j', 'gj', { noremap = true, silent = true })
-vim.keymap.set('n', 'k', 'gk', { noremap = true, silent = true })
+-- -- create remap for JK for moving entire unbroken line
+-- vim.keymap.set('n', 'J', 'j', { noremap = true, silent = true })
+-- vim.keymap.set('n', 'K', 'k', { noremap = true, silent = true })
+--
+-- vim.keymap.set('n', 'j', 'gj', { noremap = true, silent = true })
+-- vim.keymap.set('n', 'k', 'gk', { noremap = true, silent = true })
 
 vim.opt.linebreak = true
+vim.opt.tw = 80
+
